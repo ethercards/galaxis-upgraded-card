0e01f0fce077c0e670ac58f7b1337b62a458adfd
Zoltan Tasnadi <>
2022-03-28 11:33:22 +0200

    test

---
+++ dist/index.esm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+var GalaxisCard = function GalaxisCard(props) {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, "CARD COMPONENT");
+};
+
+export { GalaxisCard };
