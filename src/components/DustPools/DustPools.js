224ccf38dc1ea1cd663bd01c1359e5d5ac3d69ba
Zoltan Tasnadi <>
2022-04-25 11:30:58 +0200

    Dust pools

---
+++ src/components/DustPools/DustPools.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+
+
+const DustPools = (props)=>{
+
+
+
+
+	return (
+		<>
+			Pools
+		</>
+	);
+}
+
+export default DustPools;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as GalaxisCard } from './components/GalaxisCard';
+export { default as DustPools } from './components/DustPools/DustPools';
