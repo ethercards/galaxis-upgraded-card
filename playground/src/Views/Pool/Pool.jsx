a45b74715841aca52cb0fa43a9afc832382d3c92
Zoltan Tasnadi <>
2022-04-26 16:02:16 +0200

    theme upd

--- playground/src/Views/Pool/Pool.jsx
+++ playground/src/Views/Pool/Pool.jsx
@@ -1,12 +1,34 @@
+import { Box } from '@mui/system';
 import { DustPools } from 'galaxis-components';
+import Header from '../../components/Header';
 
 const PoolTest = ()=>{
 
+	const sx = {
+		root: {
+			position: 'relative',
+			width: '100%',
+			pt: '120px',
+			pb: '115px',
+			mb: '100px',
+			overflow: 'hidden',
+			transition: 'all .3s',
+			backgroundColor: '#FAF9F5',
+			px: '20px',
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			alignItems: 'center',
+		}
+	};
 
 
 	return(
 		<>
-			<DustPools/>
+			<Header/>
+			<Box sx={sx.root} id="homeContainer">	
+				<DustPools/>
+			</Box>
 		</>
 	);
 }
